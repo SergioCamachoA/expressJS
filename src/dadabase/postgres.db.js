ac65324b981872fdb5e7235b9e72f072a246d2c7
Sergio Camacho <ser.a.camacho93@protonmail.com>
2021-09-23 20:42:29 -0500

    heroku initial setup

--- src/dadabase/postgres.db.js
+++ src/dadabase/postgres.db.js
@@ -2,10 +2,20 @@ import Sequelize from "sequelize"
 
 import config from "../config/index"
 
+import fs from "fs"
+
 const db = new Sequelize(config.DB_DATABASE, config.DB_USER, config.DB_PASS, {
-  host: "localhost",
-  port: 5555,
+  host: "db-postgresql-nyc3-77003-do-user-9892941-0.b.db.ondigitalocean.com",
+  // host: "localhost",
+  port: 25060,
+  // port: 5555,
   dialect: "postgres",
+  dialectOptions: {
+    bigNumberStrings: true,
+    ssl: {
+      ca: fs.readFileSync(__dirname + "/ca-certificate.cer"),
+    },
+  },
   pool: {
     max: 5,
     min: 0,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import app from "./app"
 // require("dotenv").config()
 
-const PORT = 3000
+// const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const main = async () => {
   await app.listen(PORT)
